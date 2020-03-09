80112fc2cbea4c490925c6ab360e77ee0abedaf7
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-03-09 14:43:00 +0300

    follow button in messages module

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -3,11 +3,13 @@
   <h4 class="alert-heading">{{alert.header}}</h4>
   <p>{{alert.body}}</p>
   <hr>
-  <p class="mb-0"><button @click="closebtn" class="btn btn-primary">Close</button>{{alert.status}}</p>
+  <p class="mb-0"><button @click="closebtn" class="btn btn-primary">Close</button> {{alert.status}}</p>
 </div>
 </template>
 <style lang="scss">
-
+  button {
+      margin-right:25px;  
+  }
 </style>
 <script>
 export default {
