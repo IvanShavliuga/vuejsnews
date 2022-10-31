c311e2b88f5cc27cf353c7b5df5dfe96b54f33b1
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2022-10-31 19:06:55 +0300

    api server worked

--- src/server/api.js
+++ src/server/api.js
@@ -1,33 +1,33 @@
-import Vue from "vue";
+import axios from "axios";
 // import VueResource from "vue-resource";
 // Vue.use(VueResource);
 // const ListResource = Vue.resource("http://localhost:3000/users{/id}");
 
 export default {
   fetchUsersList: () => {
-    return Vue.http.get("http://localhost:3000/users");
+    return axios.get("http://localhost:3000/users");
   },
   fetchMessagesList: () => {
-    return Vue.http.get("http://localhost:3000/messages");
+    return axios.get("http://localhost:3000/messages");
   },
   fetchCardsList: () => {
-    return Vue.http.get("http://localhost:3000/cards");
+    return axios.get("http://localhost:3000/cards");
   },
   fetchPostsList: () => {
-    return Vue.http.get("http://localhost:3000/posts");
+    return axios.get("http://localhost:3000/posts");
   },
   fetchCommentsList: () => {
-    return Vue.http.get("http://localhost:3000/comments");
+    return axios.get("http://localhost:3000/comments");
   },
   fetchAlert: () => {
-    return Vue.http.get("http://localhost:3000/alert");
+    return axios.get("http://localhost:3000/alert");
   },
   fetchGroupsList: () => {
-    return Vue.http.get("http://localhost:3000/groups");
+    return axios.get("http://localhost:3000/groups");
   },
   getUser: (user) => {
-    // Vue.http.patch(`http://localhost:3000/users`, { login: user.login, password: user.password }).then(d=>console.log(d.data))
-    return Vue.http.get(
+    // axios.patch(`http://localhost:3000/users`, { login: user.login, password: user.password }).then(d=>console.log(d.data))
+    return axios.get(
       `http://localhost:3000/users?login=${user.login}&password=${user.password}`
     );
   },
